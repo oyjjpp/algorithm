af2b9fae80483126fc95aefba01f62586b0aa479
oyjjpp <ouyangjuncust@aliyun.com>
2020-10-22 08:01:13 +0800

    空二叉树，返回其最大路径和

--- leetcode/0124/0124.go
+++ leetcode/0124/0124.go
@@ -1,6 +1,10 @@
 // 二叉树中的最大路径和
 package leetcode
 
+import (
+	"log"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -12,15 +16,27 @@ func maxPathSum(root *TreeNode) int {
 	maxPath(root, &maxSum)
 	return maxSum
 }
-
+/*
+data1 := &TreeNode{
+	Val:  -10,
+	Left: &TreeNode{Val: 9},
+	Right: &TreeNode{
+		Val:   20,
+		Left:  &TreeNode{Val: 15},
+		Right: &TreeNode{Val: 7},
+	},
+}
+*/
 func maxPath(root *TreeNode, maxSum *int) int {
 	if root == nil {
 		return 0
 	}
 
-	left := max(0, maxPathSum(root.Left))
-	right := max(0, maxPathSum(root.Right))
+	left := max(0, maxPath(root.Left, maxSum))
+	right := max(0, maxPath(root.Right, maxSum))
+	temp := *maxSum
 	*maxSum = max(*maxSum, (left + right + root.Val))
+	log.Println(root.Val, temp, *maxSum, (left + right + root.Val))
 	return max(left, right) + root.Val
 }
 
