3f440008ffc6e2c3839e7d36126577fa9a2ff90b
oyjjpp <ouyangjuncust@aliyun.com>
2020-10-25 20:01:36 +0800

    根据一棵树的前序遍历与中序遍历构造二叉树

--- leetcode/0105/0105.go
+++ leetcode/0105/0105.go
@@ -1,8 +1,8 @@
 package leetcode
 
 type TreeNode struct {
-	Val int
-	Left *TreeNode
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
@@ -11,22 +11,22 @@ type TreeNode struct {
 // @param preorder 前序遍历参数  中左右
 // @param inorder 中序遍历参数	左中右
 func buildTree(preorder []int, inorder []int) *TreeNode {
-    if len(preorder) == 0 {
-        return nil
-    }
-    // 前序遍历第一个元素为根节点
-    root := &TreeNode{preorder[0], nil, nil}
-    
-    // 在中序遍历中查找根节点位置
-    i := 0
-    for ; i < len(inorder); i++ {
-        if inorder[i] == preorder[0] {
-            break
-        }
-    }
-    
-    // 递归构建左右子树
-    root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
-    root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
-    return root
+	if len(preorder) == 0 {
+		return nil
+	}
+	// 前序遍历第一个元素为根节点
+	root := &TreeNode{preorder[0], nil, nil}
+
+	// 在中序遍历中查找根节点位置
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+
+	// 递归构建左右子树
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	return root
 }
--- leetcode/0105/0105_test.go
+++ leetcode/0105/0105_test.go
@@ -1,16 +1,16 @@
 package leetcode
 
 import (
-	"testing"
 	"encoding/json"
+	"testing"
 )
 
-func TestBuildTree(t *testing.T){
+func TestBuildTree(t *testing.T) {
 	preorder := []int{3, 9, 20, 15, 7}
 	inorder := []int{9, 3, 15, 20, 7}
-	
+
 	data := buildTree(preorder, inorder)
-	rs,err := json.Marshal(*data)
+	rs, err := json.Marshal(*data)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
