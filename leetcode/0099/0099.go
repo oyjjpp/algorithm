da636ebeca1fd7142b0aa919c34b1f8f81d59fc1
oyjjpp <ouyangjuncust@aliyun.com>
2020-11-02 08:45:13 +0800

    二叉搜索树中的两个节点被错误地交换

--- leetcode/0099/0099.go
+++ leetcode/0099/0099.go
@@ -1,13 +1,19 @@
 package leetcode
 
+import(
+	"log"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
+
 // recoverTree
 // 恢复一个二叉搜索树
 func recoverTree(root *TreeNode)  {
+	// 定义一个数组保存中序遍历结果
     nums := []int{}
     
     // 定义一个中序遍历函数，将结果保存到数组中
@@ -22,17 +28,19 @@ func recoverTree(root *TreeNode)  {
     }
     // 中序遍历二叉树
     inorder(root)
+    log.Println(nums)
     // 查找交换的节点
     x, y := findTwoSwapped(nums)
-    fmt.Println(x, y)
+    log.Println(x, y)
     recover(root, 2, x, y)
 }
 
 // findTwoSwapped
-// 找到两个交换的索引位置
+// 找到两个交换的索引位置值
 func findTwoSwapped(nums []int) (int, int) {
     x, y := -1, -1
     for i := 0; i < len(nums) - 1; i++ {
+		// TODO 两个错误的位置一定是相连的吗？
         if nums[i + 1] < nums[i] {
             y = nums[i+1]
             if x == -1 {
@@ -45,10 +53,14 @@ func findTwoSwapped(nums []int) (int, int) {
     return x, y
 }
 
+// 恢复二叉搜索树
+// recover
+// @param root 二叉树
 func recover(root *TreeNode, count, x, y int) {
     if root == nil {
         return
     }
+    // 二叉树中查找对应的值然后进行替换
     if root.Val == x || root.Val == y {
         if root.Val == x {
             root.Val = y
@@ -60,6 +72,6 @@ func recover(root *TreeNode, count, x, y int) {
             return
         }
     }
-    recover(root.Right, count, x, y)
     recover(root.Left, count, x, y)
+    recover(root.Right, count, x, y)
 }
--- leetcode/0099/0099_test.go
+++ leetcode/0099/0099_test.go
@@ -1,1 +1,38 @@
 package leetcode
+
+import(
+	"testing"
+	"log"
+)
+
+func TestRecoverTree(t *testing.T){
+	data := &TreeNode{
+		Val:3,
+		Left:&TreeNode{Val:1},
+		Right:&TreeNode{Val:4,Left:&TreeNode{Val:2}},
+	}
+	recoverTree(data)
+	
+	// 中序遍历
+	inorder(data)
+}
+
+// 中序遍历
+// inorder
+func inorder(root *TreeNode){
+	if root==nil{
+		return
+	}
+	inorder(root.Left)
+	log.Println(root.Val)
+	inorder(root.Right)
+}
+
+func TestInorder(t *testing.T){
+	data := &TreeNode{
+		Val:1,
+		Left:&TreeNode{Val:2,Left:&TreeNode{Val:21}},
+		Right:&TreeNode{Val:3},
+	}
+	inorder(data)
+}
