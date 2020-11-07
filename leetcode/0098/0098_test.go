3f66bc867f5d2fa8f58e5955b9507f8298b41d41
oyjjpp <ouyangjuncust@aliyun.com>
2020-11-07 10:35:30 +0800

    修剪二叉搜索树

--- leetcode/0098/0098_test.go
+++ leetcode/0098/0098_test.go
@@ -8,15 +8,15 @@ import (
 // [5,1,4,null,null,3,6]
 func TestIsValidBST(t *testing.T) {
 
-	data := &TreeNode{
-		Val:  5,
-		Left: &TreeNode{Val: 1},
-		Right: &TreeNode{
-			Val:   4,
-			Left:  &TreeNode{Val: 3},
-			Right: &TreeNode{Val: 6},
-		},
-	}
+	// data := &TreeNode{
+	// 	Val:  5,
+	// 	Left: &TreeNode{Val: 1},
+	// 	Right: &TreeNode{
+	// 		Val:   4,
+	// 		Left:  &TreeNode{Val: 3},
+	// 		Right: &TreeNode{Val: 6},
+	// 	},
+	// }
 
 	/*
 		data := &TreeNode{
@@ -32,6 +32,16 @@ func TestIsValidBST(t *testing.T) {
 			Left:&TreeNode{Val:1},
 		}
 	*/
+
+	data := &TreeNode{
+		Val:  2,
+		Left: &TreeNode{Val: 1},
+		Right: &TreeNode{
+			Val:   4,
+			Left:  &TreeNode{Val: 3},
+			Right: &TreeNode{Val: 6},
+		},
+	}
 	rs := isValidBST(data)
 	log.Println(rs)
 }
--- leetcode/0669/0669_test.go
+++ leetcode/0669/0669_test.go
@@ -1,1 +1,45 @@
 package leetcode
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+func TestTrimBST(t *testing.T) {
+	// TODO 为什么不是二叉搜索树？？？ 尴尬了 原数据不是二叉搜索树
+	// data := &TreeNode{
+	// 	Val:  5,
+	// 	Left: &TreeNode{Val: 1},
+	// 	Right: &TreeNode{
+	// 		Val:   4,
+	// 		Left:  &TreeNode{Val: 3},
+	// 		Right: &TreeNode{Val: 6},
+	// 	},
+	// }
+	// TODO 验证结果
+	data := &TreeNode{
+		Val:  2,
+		Left: &TreeNode{Val: 1},
+		Right: &TreeNode{
+			Val:   4,
+			Left:  &TreeNode{Val: 3},
+			Right: &TreeNode{Val: 6},
+		},
+	}
+	inorder(data)
+	trimBST(data, 3, 5)
+	log.Println("修剪之后")
+	inorder(data)
+	rs, _ := json.Marshal(data)
+	log.Println(string(rs))
+}
+
+func inorder(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	inorder(root.Left)
+	log.Println(root.Val)
+	inorder(root.Right)
+}
