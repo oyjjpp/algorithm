1c5c8093efe6730c3620cf0253230be29d941114
oyjjpp <ouyangjuncust@aliyun.com>
2020-11-28 09:51:23 +0800

    二分搜索

--- leetcode/0034/0034.go
+++ leetcode/0034/0034.go
@@ -3,59 +3,82 @@ package leetcode
 // searchRange
 // 在排序数组中查找元素的第一个和最后一个位置
 func searchRange(nums []int, target int) []int {
-    if len(nums) == 0 {
-        return []int{-1, -1}
-    }
-    rs := []int{}
-    left, right := 0, len(nums)
-    
-    for left < right {
-        mid := (left + right)/2
-        
-        if nums[mid] == target {
-            right = mid
-        } else if nums[mid] < target {
-            left = mid + 1
-        } else if nums[mid] > target {
-            right = mid
-        }
-    }
-    // 左侧边际
-    if left == len(nums) || nums[left] != target {
-        return []int{-1, -1}
-    }
-    rs = append(rs, left)
-    for i:=left; i<len(nums)-1; i++ {
-        if nums[i] == target{
-            right = i
-        }else {
-            break
-        }
-    }
-    rs = append(rs, right)
-    return rs
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+	rs := []int{}
+	left, right := 0, len(nums)
+
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			right = mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		}
+	}
+	// 左侧边际
+	if left == len(nums) || nums[left] != target {
+		return []int{-1, -1}
+	}
+	rs = append(rs, left)
+	for i := left; i < len(nums); i++ {
+		if nums[i] == target {
+			right = i
+		} else {
+			break
+		}
+	}
+	rs = append(rs, right)
+	return rs
 }
 
 // leftSearch
 // 寻找左侧边际的二分查找
 func leftSearch(nums []int, target int) int {
-    if len(nums) == 0 {
-        return -1
-    }
-    left, right := 0, len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] < target {
-            left = mid + 1
-        } else if nums[mid] > target {
-            right = mid - 1
-        }else if nums[mid] == target {
-            right = mid - 1
-        }
-    }
-    
-    if left == len(nums) || nums[left] != target {
-        return -1
-    }
-    return left
+	if len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] == target {
+			right = mid - 1
+		}
+	}
+
+	// 越界检查
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
+}
+
+// rightSrache
+// 寻找右侧边际的二分查找
+func rightSrache(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}else if nums[mid] = target{
+			left = mid+1
+		}
+	}
+	if right <0 || nums[right] != target{
+		return -1
+	}
+	return right
 }
--- leetcode/0034/0034_test.go
+++ leetcode/0034/0034_test.go
@@ -1,14 +1,17 @@
 package leetcode
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestSearchRange(t *testing.T){
-    data := []int{5,7,7,8,8,10}
-    rs := searchRange(data, 8)
-    t.Log(rs)
-    
-    rs1 := searchRange(data, 6)
-    t.Log(rs1)
+func TestSearchRange(t *testing.T) {
+	data := []int{5, 7, 7, 8, 8, 10}
+	rs := searchRange(data, 8)
+	t.Log(rs)
+
+	rs1 := searchRange(data, 6)
+	t.Log(rs1)
+
+	rs2 := searchRange([]int{2, 2}, 2)
+	t.Log(rs2)
 }
