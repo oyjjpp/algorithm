574745364be8ec7cae3ce00d9987b5fc99540a22
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-08 08:12:34 +0800

    673 最长子序列个数

---
+++ leetcode/0491/0491.go
@@ -0,0 +1,10 @@
+package leetcode
+
+// findSubsequences
+// 递增子序列
+func findSubsequences(nums []int) [][]int {
+    data := [][]int{}
+    if len(nums) == 0 {
+        return data
+    }
+}
---
+++ leetcode/0673/0673_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import(
+    "testing"
+)
+
+func TestSlice(t *testing.T){
+    data := []int{1, 2, 3}
+    t.Log(data[len(data)-1])
+}
+
+func TestFindMaxLengthOfLIS(t *testing.T){
+    data := []int{1,3,5,4,7}
+    rs := findMaxLengthOfLIS(data)
+    t.Log(rs)
+}
