a4579ad02eab62aebb8acb632ab8ef613aa35e3e
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-10 20:46:05 +0800

    1143. 最长公共子序列

---
+++ leetcode/1143/1143.go
@@ -0,0 +1,11 @@
+package leetcode
+
+// longestCommonSubsequence
+// 1143. 最长公共子序列
+func longestCommonSubsequence(text1 string, text2 string) int {
+    w := len(text1)
+    h := len(text2)
+    dp :=[w][h]int{}
+    log.Println(dp)
+    return 0
+}
