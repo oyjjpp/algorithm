9608c3d6d2e348a9bc7acfed21dce6c59f75b221
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-15 08:10:57 +0800

    0072-编辑距离

--- leetcode/0072/0072.go
+++ leetcode/0072/0072.go
@@ -1,18 +1,31 @@
 package leetcode
 
-import "log"
+import (
+    "log"
+    "fmt"
+)
 
 // minDistance
 // 72. 编辑距离
 // 使用递归方式解决动态规划问题
 func minDistance(word1 string, word2 string) int {
+    // 备忘录
+    memorandum := map[string]int{}
+    
 	var dp func(i, j int) int
 	dp = func(i, j int) (rs int) {
-		// TODO
+		// TODO 验证
 		defer func() {
-			log.Println(i, j, rs)
+			// log.Println(i, j, rs)
 		}()
-
+        
+        // 备忘录解决重叠子问题
+        key := fmt.Sprintf("%d%d", i, j)
+        if data, ok := memorandum[key]; ok {
+            log.Println(key)
+            return data
+        }
+        
 		// base case
 		// 当一个字符串已经全部扫描完的情况，则直接累加另一个字符串剩余的长度
 		if i == -1 {
@@ -28,10 +41,12 @@ func minDistance(word1 string, word2 string) int {
 		// 两个字符串相等情况，则直接跳过
 		if word1[i] == word2[j] {
 			rs = dp(i-1, j-1)
+            memorandum[key] = rs
 			return
 		}
 		// 插入/删除/替换
 		rs = min((dp(i, j-1) + 1), (dp(i-1, j) + 1), (dp(i-1, j-1) + 1))
+        memorandum[key] = rs
 		return
 	}
 
@@ -49,4 +64,47 @@ func min(a, b, c int) int {
 		minNums = c
 	}
 	return minNums
+}
+
+// minDistanceV
+// 编辑距离
+// 使用DP table
+func minDistanceV(word1 string, word2 string) int {
+    m := len(word1)
+    n := len(word2)
+    // 创建DP数组存储切片
+    dp := createSlice(m+1, n+1)
+    
+    // base case
+    for i := 1; i <= m; i++ {
+        dp[i][0] = i
+    }
+    
+    for i := 1; i <= n; i++ {
+        dp[0][i] = i
+    }
+    
+    // 自底向上求解
+    for i := 1 ;i <=m; i++ {
+        for j := 1; j <= n; j ++ {
+            if word1[i-1] == word2[j-1] {
+                dp[i][j] = dp[i-1][j-1]
+            }else{
+                dp[i][j] = min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]+1)
+            }
+        }
+    }
+    log.Println(dp)
+    return dp[m][n]
+}
+
+// createSlice
+// 创建二维切片
+func createSlice(m, n int) [][]int{
+    data := make([][]int, m)
+    for key:= range data{
+        data[key] = make([]int, n)
+    }
+    return data
+    
 }
--- leetcode/0072/0072_test.go
+++ leetcode/0072/0072_test.go
@@ -9,7 +9,38 @@ func TestMinDistance(t *testing.T) {
 	rs := minDistance(word1, word2)
 	t.Log(rs)
 
+	// word1, word2 = "intention", "execution"
+	// rs = minDistance(word1, word2)
+	// t.Log(rs)
+    
+    // word1, word2 = "algorithm", "altruistic"
+    // rs = minDistance(word1, word2)
+	// t.Log(rs)
+    
+    word1, word2 = "dinitrophenylhydrazine", "benzalphenylhydrazone"
+    rs = minDistance(word1, word2)
+	t.Log(rs)
+}
+
+func TestMinDistanceV(t *testing.T) {
+	word1, word2 := "horse", "ros"
+	rs := minDistanceV(word1, word2)
+	t.Log(rs)
+
 	word1, word2 = "intention", "execution"
 	rs = minDistance(word1, word2)
 	t.Log(rs)
+    
+    word1, word2 = "algorithm", "altruistic"
+    rs = minDistance(word1, word2)
+	t.Log(rs)
+    
+    word1, word2 = "dinitrophenylhydrazine", "benzalphenylhydrazone"
+    rs = minDistanceV(word1, word2)
+	t.Log(rs)
+}
+
+func TestCreateSlice(t *testing.T){
+    data := createSlice(3, 2)
+    t.Log(data)
 }
