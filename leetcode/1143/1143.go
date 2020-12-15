aa843b5dca3b3bcb20af547468369c7d0d9c696f
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-15 16:39:31 +0800

    最长公共子序列

--- leetcode/1143/1143.go
+++ leetcode/1143/1143.go
@@ -1,11 +1,46 @@
 package leetcode
 
+import(
+    "log"
+)
+
 // longestCommonSubsequence
 // 1143. 最长公共子序列
 func longestCommonSubsequence(text1 string, text2 string) int {
     w := len(text1)
     h := len(text2)
-    dp :=[w][h]int{}
+    
+    // base case
+    // 初始化
+    dp := MakeIntSlice(w+1, h+1)
     log.Println(dp)
-    return 0
+    
+    for i:=1; i<=w; i++ {
+        for j:=1; j<=h; j++ {
+            // 状态转移
+            if text1[i-1] == text2[j-1] {
+                dp[i][j] = dp[i-1][j-1]+1    
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[w][h]
+}
+
+func max(a, b int) int{
+    if a > b {
+        return a
+    }
+    return b
+}
+
+// MakeSlice
+// 创建二维切片
+func MakeIntSlice(row, column int) [][]int {
+    data := make([][]int, row)
+    for index := range data {
+        data[index] = make([]int, column)
+    }
+    return data
 }
--- leetcode/1143/1143_test.go
+++ leetcode/1143/1143_test.go
@@ -1,1 +1,12 @@
 package leetcode
+
+import(
+    "testing"
+)
+
+func TestLongestCommonSubsequence(t *testing.T){
+    text1 := "abcde" 
+    text2 := "ace" 
+    rs := longestCommonSubsequence(text1, text2)
+    t.Log(rs)
+}
