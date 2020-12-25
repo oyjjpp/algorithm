323be6fd88b40e2df5b73b87158b7fe1baa389fc
oyjjpp <ouyangjuncust@aliyun.com>
2020-12-25 08:47:09 +0800

    651 四键键盘

--- leetcode/0651/0651.go
+++ leetcode/0651/0651.go
@@ -1,1 +1,23 @@
 package leetcode
+
+func maxA(n int) int {
+    var dp func(n, sumA, cacheA int) int
+    dp = func(n, sumA, cacheA int) int {
+        if n < 0 {
+            return sumA
+        }
+        a := dp(n-1, sumA+1, cacheA) 
+        b := dp(n-1, sumA+cacheA, cacheA)
+        c := dp(n-2, sumA, sumA)
+        sumA = max(max(a, b), c)
+        return sumA 
+    }
+    return dp(n, 0, 0)
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
--- leetcode/0651/0651_test.go
+++ leetcode/0651/0651_test.go
@@ -1,1 +1,10 @@
 package leetcode
+
+import (
+    "testing"
+)
+
+func TestMaxA(t *testing.T){
+    rs := maxA(3)
+    t.Log(rs)
+}
