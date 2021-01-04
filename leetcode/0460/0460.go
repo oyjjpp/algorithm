3747066619905ac37c39ce181c74da35c29bad87
oyjjpp <ouyangjuncust@aliyun.com>
2021-01-05 07:58:23 +0800

    460-LFU

--- leetcode/0460/0460.go
+++ leetcode/0460/0460.go
@@ -1,94 +1,127 @@
 package leetcode
 
+import (
+	"container/list"
+)
+
+type Node struct {
+	key  int
+	val  int
+	freq int
+}
+
 type LFUCache struct {
 	// key 到val的映射
-	keyToVal map[int]int
-	// key到freq的映射
-	keyToFreq map[int]int
+	keyToVal map[int]*list.Element
 	// freq到key列表的映射
-	freqTokeys map[int][]int
+	freqTokeys map[int]*list.List
 	minFreq    int
 	cap        int
 }
 
+// Constructor
+// 初始化函数
 func Constructor(capacity int) LFUCache {
 	return LFUCache{
-		keyToVal:   map[int]int{},
-		keyToFreq:  map[int]int{},
-		freqTokeys: map[int][]int{},
+		keyToVal:   map[int]*list.Element{},
+		freqTokeys: map[int]*list.List{},
 		minFreq:    0,
 		cap:        capacity,
 	}
 }
 
+// Get
+// 获取值
 func (this *LFUCache) Get(key int) int {
 	// 不存在时则返回-1
-	data, ok := this.keyToFreq[key]
+	ele, ok := this.keyToVal[key]
 	if !ok {
 		return -1
 	}
 	// 增加key对应的freq
-	return data
+	return this.updateFreq(ele)
 }
 
+// updateFreq
+// 访问key的同时需要增加对应freq的次数
+func (this *LFUCache) updateFreq(ele *list.Element) int {
+	data := ele.Value.(*Node)
+	// 处理freq to key的映射关系
+	curList, ok := this.freqTokeys[data.freq]
+	if !ok {
+		return -1
+	}
+	curList.Remove(ele)
+
+	// TODO 确认是否需要更新
+	if curList.Len() == 0 {
+		if data.freq == this.minFreq {
+			this.minFreq++
+		}
+	}
+	// 更新key与freq的关系，当key对应的freq增加时候，则存储到对应的链表中
+	data.freq++
+	newList, ok := this.freqTokeys[data.freq]
+	// 如果不存在链表则创建
+	if !ok {
+		newList = list.New()
+		this.freqTokeys[data.freq] = newList
+	}
+	newEle := newList.PushBack(data)
+
+	// 更新元素的freq
+	this.keyToVal[data.key] = newEle
+	return data.val
+}
+
+// Put
+// 存入新的值
 func (this *LFUCache) Put(key int, value int) {
 	if this.cap <= 0 {
 		return
 	}
 	// 如果key已存在，修改对应的val即可
-	if _, ok := this.keyToVal[key]; ok {
-		// 更改key的值
-		this.keyToVal[key] = value
+	if ele, ok := this.keyToVal[key]; ok {
+		data := ele.Value.(*Node)
+		data.val = value
 		// 调整key的freq次数
-		this.increaseFreq(key)
+		this.updateFreq(ele)
 		return
 	}
 
 	// key不存在则需要插入
 	// 容量已满的话需要淘汰一个frqp最小的key
-	if this.cap <= len(this.keyToVal) {
+	if this.cap == len(this.keyToVal) {
 		this.removeMinFreqKey()
 	}
 
-	// 插入key和val，对应的freq为1
-	this.keyToVal[key] = value
-	this.keyToFreq[key] = 1
-
-	// 更新freq到key列表的映射
-	if data, ok := this.freqTokeys[1]; ok {
-		data = append(data, key)
-	} else {
-		this.freqTokeys[1] = []int{key}
+	// 创建新的节点
+	newNode := &Node{
+		key:  key,
+		val:  value,
+		freq: 1,
 	}
-
-	// 插入新的key后最小分freq肯定是1
-	this.minFreq = 1
-}
-
-// increaseFreq
-// 增加对应key的freq
-func (this *LFUCache) increaseFreq(key int) {
-	// 更新key to freq
-	freq := this.keyToFreq[key]
-	this.keyToFreq[key] = freq + 1
-
-	// 更新freq to key
+	// 更新freq to key的链表
+	newList, ok := this.freqTokeys[newNode.freq]
+	if !ok {
+		// 不存在则创建一个链表
+		newList = list.New()
+		this.freqTokeys[newNode.freq] = newList
+	}
+	newEle := newList.PushBack(newNode)
+	this.keyToVal[key] = newEle
+	this.minFreq = newNode.freq
 }
 
 // removeMinFreqKey
 // 淘汰一个freq最小的key
 func (this *LFUCache) removeMinFreqKey() {
-	// freq最小的key列表
-	keyList := this.freqTokeys[this.minFreq]
-
-	// 最先被插入的key就是应该被淘汰的key
-	deleteKey := keyList[0]
-	if len(keyList) == 1 {
-		delete(this.freqTokeys, this.minFreq)
-	} else {
-		// TODO 待确定是否能修改原来切片
-		keyList = keyList[1:]
+	minList, ok := this.freqTokeys[this.minFreq]
+	if !ok {
+		return
 	}
-	delete(this.keyToFreq, deleteKey)
-	delete(this.keyToVal, deleteKey)
+	delE := minList.Front()
+	minList.Remove(delE)
+	data := delE.Value.(*Node)
+	delete(this.keyToVal, data.key)
 }
--- leetcode/0460/0460_test.go
+++ leetcode/0460/0460_test.go
@@ -1,1 +1,43 @@
 package leetcode
+
+import "testing"
+
+func TestLFUCache(t *testing.T) {
+
+	// 	输入：
+	// ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+	// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+	// 输出：
+	// [null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+	// 解释：
+	// LFUCache lFUCache = new LFUCache(2);
+	// lFUCache.put(1, 1);
+	// lFUCache.put(2, 2);
+	// lFUCache.get(1);      // 返回 1
+	// lFUCache.put(3, 3);   // 去除键 2
+	// lFUCache.get(2);      // 返回 -1（未找到）
+	// lFUCache.get(3);      // 返回 3
+	// lFUCache.put(4, 4);   // 去除键 1
+	// lFUCache.get(1);      // 返回 -1（未找到）
+	// lFUCache.get(3);      // 返回 3
+	// lFUCache.get(4);      // 返回 4
+
+	lfuCache := Constructor(2)
+	lfuCache.Put(1, 1)
+	lfuCache.Put(2, 2)
+	rs := lfuCache.Get(1)
+	t.Log(rs)
+	lfuCache.Put(3, 3)
+	rs = lfuCache.Get(2)
+	t.Log(rs)
+	rs = lfuCache.Get(3)
+	t.Log(rs)
+	lfuCache.Put(4, 4)
+	rs = lfuCache.Get(1)
+	t.Log(rs)
+	rs = lfuCache.Get(3)
+	t.Log(rs)
+	rs = lfuCache.Get(4)
+	t.Log(rs)
+}
