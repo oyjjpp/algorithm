f330ebc0a417ef176afdf6c546744f3ff9532b85
oyjjpp <ouyangjuncust@aliyun.com>
2021-09-18 08:39:38 +0800

    二分法

--- common/division_search.go
+++ common/division_search.go
@@ -18,3 +18,50 @@ func binarySearch(data []int, target  int) int {
 	return -1;
 }
 
+// 二分法下界（重复元素 第一个元素）
+func binarySearchV1(data []int, target  int) int {
+	left,right := 0 ,len(data)
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if data[mid]>target {
+			right = mid-1
+		}else if data[mid]<target {
+			left = mid+1
+		}else{
+			if mid ==0 || data[mid-1]!=target{
+				return mid
+			}else{
+				right = mid-1
+			}
+		}
+	}
+
+	return -1
+}
+
+// 二分法上界（重复元素 最后一个元素）
+func binarySearchV2(data []int, target  int) int {
+	left,right := 0 ,len(data)
+
+	length := right
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if data[mid]>target {
+			right = mid-1
+		}else if data[mid]<target {
+			left = mid+1
+		}else{
+			if mid ==length-1 || data[mid+1]!=target{
+				return mid
+			}else{
+				left = mid+1
+			}
+		}
+	}
+
+	return -1
+}
--- common/division_search_test.go
+++ common/division_search_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestBinarySearch(t *testing.T){
-	data := []int{1,3,6,8,10}
-	rs := binarySearch(data,6)
+	data := []int{1,3,6,6,6,8,10}
+	rs := binarySearchV1(data,6)
 	t.Log(rs)
 }
