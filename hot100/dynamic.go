576b01117464d2df75165adff629dfa00d9034b2
ouyangjun <ouyangjuncust@aliyun.com>
2023-04-20 14:23:34 +0800

    股票问题

--- hot100/dynamic.go
+++ hot100/dynamic.go
@@ -116,3 +116,109 @@ func coinChangeV(coins []int, amount int) int {
 	}
 	return dp[amount]
 }
+
+// 状态 、 选择
+// 状态 -> 选择 （穷举）
+
+func maxProfit(prices []int) int {
+	var max func(a, b int) int
+	max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	k := 2
+	days := len(prices)
+	data := make([][][]int, days)
+	for i := range data {
+		data[i] = make([][]int, k+1)
+		for j := range data[i] {
+			data[i][j] = make([]int, 2)
+		}
+	}
+
+	for i := 0; i < days; i++ {
+		if i == 0 {
+			data[i][k][0] = 0
+			data[i][k][1] = -prices[i]
+			continue
+		}
+
+		data[i][k][0] = max(data[i-1][k][0], data[i-1][k][1]+prices[i])
+		data[i][k][1] = max(data[i-1][k][1], data[i-1][k-1][0]-prices[i])
+	}
+	return data[days-1][k][0]
+}
+
+// 注意：go 代码由 chatGPT🤖 根据我的 java 代码翻译，旨在帮助不同背景的读者理解算法逻辑。
+// 本代码还未经过力扣测试，仅供参考，如有疑惑，可以参照我写的 java 代码对比查看。
+
+// 原始版本
+func maxProfit_k_2(prices []int) int {
+
+	var max func(a, b int) int
+	max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	max_k := 2 // 最大可交易次数
+	n := len(prices)
+	dp := make([][][]int, n) // i为天数，k为当前第几次交易，0表示不持有股票，1表示持有股票
+	for i := 0; i < n; i++ {
+		dp[i] = make([][]int, max_k+1)
+		for k := 0; k < max_k+1; k++ {
+			dp[i][k] = make([]int, 2)
+		}
+	}
+	for i := 0; i < n; i++ {
+		for k := max_k; k >= 1; k-- {
+			if i-1 == -1 {
+				// 处理 base case
+				dp[i][k][0] = 0
+				dp[i][k][1] = -prices[i]
+				continue
+			}
+			dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1]+prices[i])
+			dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i])
+		}
+	}
+	// 穷举了 n × max_k × 2 个状态，正确。
+	return dp[n-1][max_k][0] //返回最大利润
+}
+
+func maxProfit_v(prices []int) int {
+	var max func(a, b int) int
+	max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	k := 2
+	n := len(prices)
+	dp := make([][][]int, n)
+	for i := range dp {
+		dp[i] = make([][]int, k+1) // 构建 3 维 DP 数组
+		for j := range dp[i] {
+			dp[i][j] = make([]int, 2) // 初始化 DP 数组
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := k; j >= 1; j-- {
+			if i-1 == -1 {
+				// 处理 base case
+				dp[i][j][0] = 0
+				dp[i][j][1] = -prices[i]
+				continue
+			}
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+		}
+	}
+
+	return dp[n-1][k][0]
+}
